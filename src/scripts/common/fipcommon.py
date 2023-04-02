13da2939834428a50f53dcdb77e47a3754c5ff02
Matteo Franzil <matteofranzil@gmail.com>
2023-04-02 12:39:55 +0200

    Fixed title

--- src/scripts/common/fipcommon.py
+++ src/scripts/common/fipcommon.py
@@ -24,48 +24,13 @@ def scrap_fip(url, mode, section):
         if div.find('a'):
             tmp.append(div.find('a')['href'])
 
-    #if "Regioni" in url:
-    #    for div in soupdesktop.find_all("a"):
-    #        if (link := div["href"].replace(" ", "%20")):
-    #            tmp.append(link)
-    #else:
-    #    if "Documento.asp" in url:
-    #        subclass = "NewsBox LIST"
-    #    elif "Comunicato.asp" in url:
-    #        subclass = "listBkg"
-    #    else:
-    #        raise ValueError("Cannot look for subclass in this url")
-#
-#        for div in soupdesktop.find_all("div", attrs={"class": subclass}):
-#            if div['onclick']:
-#                try:
-#                    link = div['onclick'].split("'")[1].replace(" ", "%20")
-#                    print(link)
-#                except Exception as e:
-#                    continue
-#
-#                tmp.append(link)
-
     for link in tmp:
-        # if mode == "delibera":
-        #     if "deliber" not in link.lower():
-        #         continue
-        # elif mode == "comunicato":
-        #     if "comunicat" not in link.lower():
-        #         continue
-
         if link == "" or link is None:
             continue
 
         if not link.startswith("http"):
             log.warning(f"Invalid URL format: {link}")
 
-        #if not link.startswith("/") and not link.startswith("http"):
-        #    link = "/" + section + "/" + link
-
-        #if not link.startswith("https://www.fip.it"):
-            #link = "https://www.fip.it" + link
- 
         list_of_articles.append(link)
 
     return list_of_articles
@@ -75,10 +40,10 @@ def refresh_feed(rss_folder, request):
     url = request["url"]
     mode = request["mode"]
     section = request["section"]
-    if request["required_url_substring"] and request["required_url_substring"] is not None:
-        required_url_substring = request["required_url_substring"].lower()
-    else:
-        required_url_substring = None
+    # if request["required_url_substring"] and request["required_url_substring"] is not None:
+    #    required_url_substring = request["required_url_substring"].lower()
+    # else:
+    #     required_url_substring = None
 
     rss_file = os.path.join(rss_folder, FEED_FILENAME)
 
@@ -109,10 +74,10 @@ def refresh_feed(rss_folder, request):
         if "pdf" in urlarticolo:
             description = f"E' disponibile {request['sentences']['new_object']}" \
                 + f" per il download.\n<a href=\"{urlarticolo}\">{urlarticolo}</a>"
+            title = urlarticolo.split("/")[-1]
         else:
             description = Document(response.text).summary()
-
-        title = Document(response.text).short_title()
+            title = Document(response.text).short_title()
 
         if not title or title is None or title == "":
             title = urlarticolo
